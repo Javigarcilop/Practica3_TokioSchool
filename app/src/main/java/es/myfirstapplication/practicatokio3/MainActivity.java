306e147a2a616439f468ce4f98b6dc47b503a239
javigarcia <javigarcilop@gmail.com>
2022-11-17 10:38:51 +0100

    Cambio de propiedades en la activity_main.xml

--- app/src/main/java/es/myfirstapplication/practicatokio3/MainActivity.java
+++ app/src/main/java/es/myfirstapplication/practicatokio3/MainActivity.java
@@ -3,13 +3,52 @@ package es.myfirstapplication.practicatokio3;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+
+import com.google.android.material.snackbar.Snackbar;
+
+import es.myfirstapplication.practicatokio3.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
+
+  private ActivityMainBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+        setlisteners();
+        setvalues();
+
+
+
+
 
     }
+
+    private void setvalues() {
+
+        binding.titleLogin.setText(getString(R.string.title_main));
+        binding.buttonLogin.setText(getString(R.string.main_button_login));
+
+    }
+
+    private void setlisteners() {
+
+        binding.buttonPassword.setOnClickListener(view ->
+                Snackbar.make(view, "Se implementarán más funcionalidades próximamente",
+                        Snackbar.LENGTH_LONG).show());
+
+
+        binding.buttonAccount.setOnClickListener(view ->
+                Snackbar.make(view, "Se implementarán más funcionalidades próximamente",
+                        Snackbar.LENGTH_LONG).show());
+
+
+
+
+    }
+
 }
